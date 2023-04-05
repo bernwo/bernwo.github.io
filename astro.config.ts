a48b61679382e019575f7d2fe968880b1ce706a3
bernwo <53580103+bernwo@users.noreply.github.com>
2023-04-06 00:18:45 +0800

    Update `astro.config.ts`

--- astro.config.ts
+++ astro.config.ts
@@ -4,18 +4,20 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
+// @ts-ignore
 import footnotes from "remark-footnotes";
+// @ts-ignore
 import remarkBibtex from "@supremum/remark-bibtex";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://bernwo.github.io/",
 	markdown: {
-		syntaxHighlight: 'shiki',
+		syntaxHighlight: "shiki",
 		shikiConfig: {
-			theme: 'monokai'
+			theme: "monokai",
 		},
-		remarkPlugins: [[remarkBibtex, {bibtexFile: "./src/assets/bibfile.bib"}], footnotes],
+		remarkPlugins: [[remarkBibtex, { bibtexFile: "./src/assets/bibfile.bib" }], footnotes],
 	},
 	integrations: [
 		mdx({}),
