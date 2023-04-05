43712a0f99afb93f2a152124a3495f812779c4cf
bernwo <53580103+bernwo@users.noreply.github.com>
2023-04-06 00:19:29 +0800

    Personalise `site.config.ts`

--- src/site.config.ts
+++ src/site.config.ts
@@ -16,9 +16,9 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "WO Kah Jen",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Wo",
 	// Meta property used as a default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "WO Kah Jen's personal website",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
