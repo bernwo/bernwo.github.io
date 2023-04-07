d5a80c81618ed493137c2266d846c009f5957f8c
bernwo <53580103+bernwo@users.noreply.github.com>
2023-04-07 14:49:50 +0800

    Updates

--- src/scripts/plot.js
+++ src/scripts/plot.js
@@ -5,13 +5,22 @@ function generate_random_data(n) {
 	return Array.from({ length: n * n }, (_, i) => [~~(i / n), i % n, Math.random()]);
 }
 
-export function getBar3Doption(size) {
+function generate_sine_wave_data(n, phase) {
+	return Array.from({ length: n * n }, (_, i) => [
+		~~(i / n),
+		i % n,
+		(Math.sin(phase + (1 / 3) * Math.sqrt((~~(i / n) - n / 2) ** 2 + ((i % n) - n / 2) ** 2)) + 1) /
+			2,
+	]);
+}
+
+export function getBar3Doption(size, x_label, y_label, z_label) {
 	const textColour = darkTextColour;
 	const colorLength = 32;
 	// prettier-ignore
-	const colorScale = d3.scaleSequential(d3.interpolateRdPu).domain([0, 1]);
+	const colorScale = d3.scaleSequential(d3.interpolateMagma).domain([0, 1]);
 	// prettier-ignore
-	const colorData = Array.from({ length: colorLength }, (_, j) => colorScale(1 - j / colorLength));
+	const colorData = Array.from({ length: colorLength }, (_, j) => colorScale(j / colorLength));
 	const option = {
 		animation: false,
 		tooltip: {
@@ -30,7 +39,7 @@ export function getBar3Doption(size) {
 			minInterval: 1,
 			min: 0,
 			max: size - 1,
-			name: "i",
+			name: x_label,
 			nameTextStyle: {
 				fontSize: 24,
 				fontStyle: "italic",
@@ -42,7 +51,7 @@ export function getBar3Doption(size) {
 			minInterval: 1,
 			min: 0,
 			max: size - 1,
-			name: "j",
+			name: y_label,
 			nameTextStyle: {
 				fontSize: 24,
 				fontStyle: "italic",
@@ -55,7 +64,7 @@ export function getBar3Doption(size) {
 			minInterval: 1,
 			min: 0,
 			max: 1,
-			name: "|ρᵢⱼ|",
+			name: z_label,
 			nameTextStyle: {
 				fontSize: 33,
 				fontStyle: "italic",
@@ -79,13 +88,13 @@ export function getBar3Doption(size) {
 	return option;
 }
 
-export function getBar3Ddata(size) {
+export function getBar3Ddata(size, dataArray, name) {
 	const option = {
 		series: [
 			{
-				name: "ρᵢⱼ",
+				name: name,
 				type: "bar3D",
-				data: generate_random_data(size).map((item) => {
+				data: dataArray.map((item) => {
 					return {
 						value: [item[0], item[1], item[2]],
 					};
@@ -114,14 +123,34 @@ export function getBar3Ddata(size) {
 	return option;
 }
 
-export function createBar3D(inputID, size) {
+export function createRandomisedBar3D(inputID, size) {
 	const chartDom = document.getElementById(inputID);
 	if (chartDom != null) {
 		let myChart = echarts.init(chartDom);
-		const option = getBar3Doption(size);
-		const optionData = getBar3Ddata(size);
+		const option = getBar3Doption(size,"x","y","z");
+		const optionData = getBar3Ddata(size, generate_random_data(size), "Random data");
 		myChart.setOption(option);
 		myChart.setOption(optionData);
-		return myChart;
+	}
+}
+
+export function createSineWaveBar3D(inputID, size) {
+	const chartDom = document.getElementById(inputID);
+	if (chartDom != null) {
+		let myChart = echarts.init(chartDom);
+		const option = getBar3Doption(size,"x","y","z");
+		const optionData = getBar3Ddata(size, generate_sine_wave_data(size, 0), "Sine Wave");
+		myChart.setOption(option);
+		myChart.setOption(optionData);
+		let i = 0;
+		const updateInterval = 500;
+		setInterval(() => {
+			i++;
+			myChart.setOption({
+				series: {
+					data: generate_sine_wave_data(size, -i / 2),
+				},
+			});
+		}, updateInterval);
 	}
 }
