36a0cd8ee8957d939cccc5479f0fcca90c674587
bernwo <53580103+bernwo@users.noreply.github.com>
2023-08-21 21:18:20 +0800

    Update personal details

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -16,6 +16,6 @@ export const MENU_LINKS = [
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	github: "https://github.com/bernwo",
-	email: "kjwo@outlook.sg",
+	email: "kjwo@u.nus.edu",
 	linkedin: "https://www.linkedin.com/in/kah-jen-wo",
 };
