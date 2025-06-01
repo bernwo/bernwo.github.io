189f5cde21e2b9a4f9c6ed2c963b1fc04df464c9
bernwo <53580103+bernwo@users.noreply.github.com>
2025-06-01 13:32:24 +0800

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import remarkMath from "remark-math"; // pnpm install remark-math
+import rehypeKatex from "rehype-katex"; // pnpm install rehype-katex
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
+  vite: {
     plugins: [tailwindcss()],
+  },
+  integrations: [mdx()],
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "monokai",
+      wrap: true,
+    },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
